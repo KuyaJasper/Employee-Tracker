8a1b0e1bdb0cf94ca93e42daa915a2ea977e0da6
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-04-26 13:48:12 -0400

    Commented on schema.sql, set up employeetracker.js

--- schema.sql
+++ schema.sql
@@ -1,15 +1,18 @@
+/* Created schema */
 DROP DATABASE IF EXISTS employee_trackerDB;
 
 CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
 
+/*  Creates department table*/
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT,
     department_name VARCHAR (30),
     PRIMARY KEY (id)
 )
 
+/* Creates role table*/
 CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR (30),
@@ -18,10 +21,12 @@ CREATE TABLE role(
     PRIMARY KEY (id)
 )
 
+/* Creates emplloyee table*/
 CREATE TABLE employee(
     id INT NOT NULL  AUTO_INCREMENT,
     first_name VARCHAR (30),
     last_name VARCHAR (30),
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    PRIMARY KEY (id)
 )
