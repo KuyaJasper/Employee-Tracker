95580c1a39eae12a00e1c84f802494a771105e30
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-05-01 14:09:41 -0400

    created add a department function

--- employeetracker.js
+++ employeetracker.js
@@ -18,7 +18,14 @@ inquirer
     choices: [
       'Add departments',
       'View departments',
-      'Update departments',
+      'Remove department',
+      'Add a role',
+      'View all roles',
+      'Remove a role',
+      'View all employees',
+      'Add an employee',
+      'Remove an employee',
+      'Update employee roles',
       'Leave',
     ],
   })
@@ -30,9 +37,11 @@ inquirer
       case 'View departments':
         departmentView();
         break;
-      case 'Update departments':
-        departmentUpdate();
+      case 'Remove deaparment':
+        departmentRemove();
         break;
+
+
       case 'Leave':
         endConnection();
         break;
@@ -41,22 +50,40 @@ inquirer
 
 };
 
-const departmentAdd = () => {};
+const departmentAdd = () => {
+  inquirer
+  .prompt([
+    {
+      name: "addDepartment",
+      type: "input",
+      message: "Please enter your New Department name.",
+    },
+  ])
+  .then((answer) => {
+    connection.query('INSERT INTO department SET?;',{
+      department_name: answer.addDepartment,
+    }, (err, res) => {
+        if (err) throw err;
+        console.log(res);
+       menuPrompt();
+      }
+    );
+  });
+};
 
 const departmentView = () => {
   console.log('Loading all departments...\n');
   connection.query("SELECT * FROM department;", (err,res) => {
     if (err) throw err;
     console.table(res);
+    menuPrompt();
   });
 };
 
-const departmentUpdate = () => {};
 
 const endConnection = () => {
   console.log('Ending connection..... \n');
-  console.log('|||||||||||||||||\n');
-  console.log('Thanks for visting, have a great day!!!!\n');
+  console.log('Connection ceased, have a great day!!!!\n');
   connection.end();
 };
 
