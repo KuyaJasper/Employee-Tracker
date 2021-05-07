2dd68c50d1c7d5080c523e6c8a1960f9e88a61e6
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-05-07 15:18:15 -0400

    fixed problem with departments, added roles, started employees. Fixed schema syntax issues with SQL

--- employeetracker.js
+++ employeetracker.js
@@ -37,8 +37,26 @@ inquirer
       case 'View departments':
         departmentView();
         break;
-      case 'Remove deaparment':
+      case 'Remove department':
         departmentRemove();
+        break;
+      case 'Add a role':
+        roleAdd();
+        break;
+      case 'View all roles':
+        roleView();
+        break;
+      case 'Remove a role':
+        roleRemove();
+        break;
+      case 'View all employees':
+        employeeView();
+        break;
+      case 'Add an employee':
+        employeeAdd();
+        break;
+      case 'Remove an employee':
+        employeeRemove();
         break;
 
 
@@ -50,6 +68,12 @@ inquirer
 
 };
 
+
+/* ---------- DEPARTMENT SECTION ---------- */
+
+
+
+// ADD A DEPARTMENT
 const departmentAdd = () => {
   inquirer
   .prompt([
@@ -61,16 +85,18 @@ const departmentAdd = () => {
   ])
   .then((answer) => {
     connection.query('INSERT INTO department SET?;',{
-      department_name: answer.addDepartment,
+      name: answer.addDepartment,
     }, (err, res) => {
         if (err) throw err;
-        console.log(res);
+        console.log(` \n The "${answer.addDepartment}" department has been added!!!\n`);
        menuPrompt();
       }
     );
   });
 };
 
+
+// VIEW ALL DEPARTMENTS 
 const departmentView = () => {
   console.log('Loading all departments...\n');
   connection.query("SELECT * FROM department;", (err,res) => {
@@ -80,7 +106,157 @@ const departmentView = () => {
   });
 };
 
+// REMOVE A DEPARMENT
+const departmentRemove = () => {
+  connection.query("SELECT * FROM department", (err, res) => {
+    if (err) throw err;
+    let deleteDepartment = res.map((department) => ({
+      name: `${department.name}`,
+      value: department.id,
+    }));
+    inquirer
+      .prompt([
+        {
+          name: "department",
+          type: "rawlist",
+          message: "What department would you like to remove? (USE ID NUMBER)",
+          choices: deleteDepartment,
+        },
+      ])
+      .then((answer) => {
+        connection.query(
+          "DELETE FROM department WHERE ?",
+          {
+            id: answer.department,
+          },
 
+          (err, res) => {
+            if (err) throw err;
+            console.log(`${res.affectedRows} department deleted!\n`);
+            menuPrompt();
+          }
+        );
+      });
+  });
+}
+
+
+/* ----------  ROLES SECTION ---------- */
+
+// VIEW ALL ROLES 
+const roleView = () => {
+  console.log('Loading all employee roles...\n');
+  connection.query("SELECT * FROM roles;", (err,res) => {
+    if (err) throw err;
+    console.table(res);
+    menuPrompt();
+  });
+};
+
+// ADD A DEPARTMENT
+const roleAdd = () => {
+  connection.query("SELECT * FROM department", (err, departments) => {
+    if (err) throw err;
+    let newDepartment = departments.map((department) => ({
+      name: `${department.name}`,
+      value: department.id,
+    }));
+    inquirer
+      .prompt([
+        {
+          name: "roleName",
+          type: "input",
+          message: "Please enter your Role name.",
+        },
+        {
+          name: "salary",
+          type: "input",
+          message: "Please enter salary.",
+        },
+        {
+          name: "department",
+          type: "rawlist",
+          message: "What department is your role in?",
+          choices: newDepartment,
+        },
+      ])
+      .then((answer) => {
+        connection.query(
+          "INSERT INTO roles SET ?",
+          {
+            title: answer.roleName,
+            salary: answer.salary,
+            department_id: answer.department,
+          },
+
+          (err, res) => {
+            if (err) throw err;
+            console.log(`${res.affectedRows} role inserted!\n`);
+            menuPrompt();
+          }
+        );
+      });
+  });
+};
+
+// REMOVE A ROLE
+const roleRemove = () => {
+  connection.query("SELECT * FROM roles", (err, res) => {
+    if (err) throw err;
+    let deleteroles = res.map((department) => ({
+      name: `${roles.name}`,
+      value: roles.id,
+    }));
+    inquirer
+      .prompt([
+        {
+          name: "roles",
+          type: "rawlist",
+          message: "What role would you like to remove? (USE ID NUMBER)",
+          choices: deleteDepartment,
+        },
+      ])
+      .then((answer) => {
+        connection.query(
+          "DELETE FROM roles WHERE ?",
+          {
+            id: answer.roles,
+          },
+
+          (err, res) => {
+            if (err) throw err;
+            console.log(`${res.affectedRows} role deleted!\n`);
+            menuPrompt();
+          }
+        );
+      });
+  });
+}
+
+
+
+
+
+/* ---------- EMPLOYEE SECTION ---------- */
+
+
+//VIEW EMPLOYEES
+
+const employeeView = () => {
+  console.log('Loading all employees...\n');
+  connection.query("SELECT * FROM employee;", (err,res) => {
+    if (err) throw err;
+    console.table(res);
+    menuPrompt();
+  });
+};
+
+
+
+
+
+
+//End Connection to DB
 const endConnection = () => {
   console.log('Ending connection..... \n');
   console.log('Connection ceased, have a great day!!!!\n');
--- schema.sql
+++ schema.sql
@@ -6,31 +6,31 @@ CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
 /*  Creates department table*/
-CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR (30),
-    PRIMARY KEY (id)
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
 );
 
-/* Creates role table*/
-CREATE TABLE role(
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR (30),
+
+/* Creates roles table*/
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department (id)
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
 /* Creates emplloyee table*/
-CREATE TABLE employee(
-    id INT NOT NULL  AUTO_INCREMENT,
-    first_name VARCHAR (30),
-    last_name VARCHAR (30),
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    PRIMARY KEY (id)
-    FOREIGN KEY (role_id) REFERENCES roles (id),
-    FOREIGN KEY (manager_id) REFERENCES employee (id),
-    
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id) REFERENCES roles(id),
+    FOREIGN KEY(manager_id) REFERENCES employee(id)
 );
